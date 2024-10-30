7ac2b2562a09082a3ffe983ece915fe012ecccd1
unknown <ashijo25@gmail.com>
2024-10-31 01:33:42 +0530

    added few opcode instructions

---
+++ desktop/target/debug/deps/desktop.d
@@ -0,0 +1,5 @@
+C:\Users\TS\OneDrive\Desktop\rustlearn\chip8-emulator-rust\desktop\target\debug\deps\desktop.exe: src/main.rs
+
+C:\Users\TS\OneDrive\Desktop\rustlearn\chip8-emulator-rust\desktop\target\debug\deps\desktop.d: src/main.rs
+
+src/main.rs:
